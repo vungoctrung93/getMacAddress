92fbc0fc5f9dad7b66ab29fd4b7ab92d66abadf5
Vu Ngoc Trung <vungoctrung93@gmail.com>
2025-06-29 15:21:29 +0700

    Update MacOSGetMacAddress.sh

--- MacOSGetMacAddress.sh
+++ MacOSGetMacAddress.sh
@@ -4,5 +4,5 @@ networksetup -listallhardwareports | awk '
 /Hardware Port/ {port=$3; for(i=4;i<=NF;i++) port=port " " $i}
 /Device/ {dev=$2}
 /Ethernet Address/ {
-  printf "Name: %s\nInterface: %s\nMAC address: %s\n\n", port, dev, $3
+  printf "Name: %s\nMAC address: %s\n\n", port, $3
 }'
